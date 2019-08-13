0a72017dfb1831279e308666c19c6d53bf09ff20
Ado Sibalo <asibalo@epicsysinc.com>
2019-08-13 13:29:08 -0500

    I am taking a different approach with forms - no multi-widget objects necessary! (maybe)

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,10 +1,10 @@
-from django.forms import inlineformset_factory, MultiWidget, MultiValueField, NumberInput, CharField
-from .models import AnticipatedProject, Saleslead
+from django import forms
+from .models import AnticipatedProject
 from datetime import datetime, timedelta
 from dateutil.relativedelta import *
 
 #**************************************************************
-# The desire is to have a formset with two parts: one corresponding the the AnticipatedProjects model and one to the Forecasts model
+# The desire is to have an editable grid with two parts: one corresponding the the AnticipatedProjects model and one to the Forecasts model
 # The table headers should look something like the following:
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Sales Lead ID | Cash Flow Reponsibility | Client Name | Project Alias | Subjective Probability | Model Probability | month_previous | month_current | month_one_ahead | ... | month_twelve_ahead | Total | Notes
@@ -14,41 +14,7 @@ from dateutil.relativedelta import *
 # When a user submits the form data, forecasts should save each field to a new row e.g. the month_previous and the amount to row n, month_current and amount to row n+1, etc.
 # The month will save to the DateAnticipated field and the entered amount will save to AnticipatedAmount. Per submit, 14 rows need to be created in the table.
 #**************************************************************
-class ForecastedCashFlowWidget(MultiWidget):
-    def __init__(self, *args, **kwargs):
-        super(ForecastedCashFlowWidget, self).__init__(*args, **kwargs)
-        
-        today = datetime.today()
-        start = datetime(today.year, today.month - 1, 1)
-        forecast_dates = [start] # initialize with the last month
-        # want to include previous month and the next 13
-        for i in range(1,14):
-            forecast_dates.append(start + relativedelta(months=i))
-        
-        self.widgets = [NumberInput(forecast) for forecast in forecast_dates]
-    
-    def decompress(self, value):
-        if value:
-            return [value]
-        return [None for i in range(15)]
-
-class ForecastedCashFlowField(MultiValueField):
-    widget = ForecastedCashFlowWidget
-
-    def __init__(self, *args, **kwargs):
-        super(ForecastedCashFlowField, self).__init__(*args, **kwargs)
-        fields = tuple(CharField() for i in range(1, 15))
-
-    def compress(self, data_list):
-        return poop
-
-
-ProjectFormSet = inlineformset_factory(Saleslead, 
-                                       AnticipatedProject, 
-                                       fields = ('salesleadid', 
-                                                 'cash_flow_responsible', 
-                                                 'project_name', 
-                                                 'client_name', 
-                                                 'subjective_probability', 
-                                                 'model_probability')
-                                      )
+class AnticipatedAwardForm(forms.Form):
+    cash_flow_responsiblity = forms.CharField()
+    subjective_probability = forms.IntegerField()
+    notes = forms.Textarea()
