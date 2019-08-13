2634fda744bba5ffc43914f12759f09f8cea7bea
Ado Sibalo <asibalo@epicsysinc.com>
2019-08-13 13:35:10 -0500

    extra context for the template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,34 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Saleslead, AnticipatedProject
+from django.views.generic.edit import FormView
+from .models import AnticipatedProject, Salesleadquote
+from dashboard.forms import AnticipatedAwardForm
 from datetime import datetime, timedelta
 from dateutil.relativedelta import *
 
 # Create your views here.
 def home(request):
     #context = {
-    #    "posts": Post.objects.all()
+    #    "projects": AnticipatedProjects.objects.all()
     #}
     return render(request, "dashboard/home.html")
 
-class ProjectListView(ListView):
-    model = AnticipatedProject
-    template_name = 'dashboard/home.html'
-    context_object_name = 'projects'
-    paginate_by = 25
 
+class ProjectListView(ListView):
+    queryset = Salesleadquote.rolling_fourteen_months_objects.my_custom_sql()
+    template_name = 'dashboard/home.html'
+    context_object_name = 'anticipated_projects'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         today = datetime.today()
         start = datetime(today.year, today.month - 1, 1)
-        forecast_dates = [start] # initialize with the last month
+        rolling_fourteen_months = [start] # initialize with the last month
         # want to include previous month, this month, and the next 12 in the view
         for i in range(1,13):
-            forecast_dates.append(start + relativedelta(months=i))
+            rolling_fourteen_months.append(start + relativedelta(months=i))
         
-        context["forecast_dates"] = forecast_dates
-
+        context["rolling_fourteen_months"] = [date.strftime("%Y-%m-%d") for date in rolling_fourteen_months]
+        context["keys"] = self.queryset[0].keys()
         return context
 
 
