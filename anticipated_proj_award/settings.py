9fd373d49d8e829ec206d7d91819fa8116261d0c
Ado Sibalo <asibalo@epicsysinc.com>
2019-08-19 14:55:19 -0500

    installed apps

--- anticipated_proj_award/settings.py
+++ anticipated_proj_award/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = 'static/'
 STATIC_URL = '/static/'
+
